be5bcfb414f6c80d7b73a6a1fa92d869aee2cae0
Logan Lucas <logan.lucas83@gmail.com>
2025-11-19 17:28:41 +0100

    get user input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod google;
 
+use std::io::Write;
+
 use keyring::{Entry, Error::NoEntry};
 
 fn main() {
@@ -22,17 +24,35 @@ fn main() {
 
     println!("access_token {access_token}");
 
-    match google::retreive_playlists(&client, &access_token) {
-        Ok(_) => (),
+    let playlists = match google::retreive_playlists(&client, &access_token) {
+        Ok(playlists) => playlists,
         Err(_) => {
             let refresh_token = Entry::new("yt-randomizer", "refresh").unwrap().get_password().unwrap();
             if let Ok(access_token) = google::refresh_access_token(&client, &refresh_token) {
                 keyring_entry.set_password(&access_token).unwrap();
 
-                let playlists = google::retreive_playlists(&client, &access_token).unwrap();
-
-                println!("Found {} playlists", playlists.len());
+                google::retreive_playlists(&client, &access_token).unwrap()
+            } else {
+                panic!("Error trying to refresh access token which needs to be handled and will probably be in the near future");
             }
         }
-    }
+    };
+
+    // TODO: Handle 0 playlists
+    println!("Found {} playlists", playlists.len());
+
+    let mut input = String::new();
+
+    print!("Please enter a playlist number [1-{}]: ", playlists.len());
+    std::io::stdout().flush();
+
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Error while reading line");
+
+    // TODO: watchout for 0
+    let index: usize = input.trim().parse().expect("Input is not a valid number");
+
+    let chosen_playlist = &playlists[index-1];
+    println!("Chose {}, with id: {}", chosen_playlist.snippet.title, chosen_playlist.id);
 }
